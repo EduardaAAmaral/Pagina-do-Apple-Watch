6f33f60d09d9cee2e6e8fd8d715ca30682d0d332
Eduarda A <100651298+EduardaAAmaral@users.noreply.github.com>
2024-01-30 20:07:55 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const verdeCipreste = {
+    nome: 'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste',
+};
+
+const azulInverno = {
+    nome: 'Azul-inverno',
+    pasta: 'imagens-azul-inverno',
+};
+
+const meiaNoite = {
+    nome: 'Meia-noite',
+    pasta: 'imagens-meia-noite',
+    emEstoque: false,
+};
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar',
+};
+
+const rosaClaro = {
+    nome: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro',
+};
+
+const Cores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
